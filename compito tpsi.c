9585d24441465c39834895124c558cd2645aaefd
Simo0777 <simone.damico@davincimilazzo.edu.it>
2024-12-03 13:36:53 +0100

    Add files via upload

---
+++ compito tpsi.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+int menu(void)
+
+	
+	
+	printf("+Somma\n")
+	printf("*Moltiplicazione\n");
+	printf("-Differenza\n");
+	printf("/Divisione\n");
+	printf("^Potenza\n");
+	printf("#Logger\n");
+	printf("!Esci\n");
+	
+			
+int somma(int n1, int n2){
+int som=0;
+printf("inserisci un numero:\n");
+scanf("%d",&n1);
+printf("inserisci un numero:\n");
+scanf("%d",&n2);
+tot=n1+n2;
+printf("%d+%d=%d",n1,n2,tot);
+}
+int differenza(int n1, int n2){
+	int diff=0
+int n1,n2,diff=0;
+printf("inserisci un numero\n");
+scanf("%d",&n1);
+printf("inserisci un numero\n");
+scanf("%d",&n2);
+diff=n1-n2;
+printf("%d-%d=%d",n1,n2,diff);
+}
+int moltiplicazione(int n1, int n2){
+int ris=0;
+printf("inserisci un numero\n");
+scanf("%d",&n1);
+printf("inserisci un numero\n");
+scanf("%d",&n2);
+ris=n1+n2;
+printf("%d*%d=%d",n1,n2,ris);
+}
+float divisione(float n1, float n2){
+	float div=0;
+	printf("inserisci un numero\n");
+	scanf("%d",&n1);
+	printf("inserisci un numero\n");
+	scanf("%d",&n2);
+	div=n1/n2
+	printf("%d:%d= %d2.f",n1,n2,div);
+	return n1,n2;
+}
+
+
+
+
